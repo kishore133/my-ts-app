84f305ac065589f453dfdf5ebba12647eb33260d
peddisetti venkata kishore <88092794+kishore133@users.noreply.github.com>
2025-11-07 11:47:05 +0530

    Create main.js

---
+++ dist/main.js
@@ -0,0 +1,11 @@
+"use strict";
+const greet = (name) => {
+    return `Hello, ${name}! TypeScript is running perfectly from GitHub Pages.`;
+};
+const output = document.getElementById("output");
+const btn = document.getElementById("btn");
+if (output && btn) {
+    btn.addEventListener("click", () => {
+        output.textContent = greet("Kishore");
+    });
+}
